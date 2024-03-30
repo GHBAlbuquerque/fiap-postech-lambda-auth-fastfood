31b8b7c07f46e3f21c9b210b3af9040e38260f17
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-03-30 15:38:44 -0300

    chore: muda versao terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~>3.27"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 
   required_version = ">=0.14.9"
 
-   backend "s3" {
-       bucket = "terraform-state-backend-postech"
-       key    = "arn:aws:s3:::terraform-state-backend-postech"
-       region = "east-us-1"
-   }
+  backend "s3" {
+    bucket = "terraform-state-backend-postech"
+    key    = "arn:aws:s3:::terraform-state-backend-postech"
+    region = "east-us-1"
+  }
 }
 
 
