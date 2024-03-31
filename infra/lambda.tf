6b43e31fc9ff51031f6a9b2b47e4cb458cebaed4
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-03-31 15:32:08 -0300

    infra: ajuste arquivo tf lambda

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -19,12 +19,13 @@ resource "aws_lambda_function" "postech-lambda-auth-fastfood" {
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello.function_name
+  function_name = aws_lambda_function.postech-lambda-auth-fastfood.function_name
   principal     = "apigateway.amazonaws.com"
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+  # source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+  source_arn = "*/*/*"
 }
 
 
