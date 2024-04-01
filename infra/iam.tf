4dd97bbaa0a808843b4602401676e61ca678050a
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-04-01 11:42:48 -0300

    infra: tentativa criacao nova role lambda

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,3 +17,22 @@
 #  name               = "${var.project_name}-lambda-role"
 #  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 #}
+
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
+}
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -10,7 +10,8 @@ resource "aws_lambda_function" "postech-lambda-auth-fastfood" {
 
   function_name = var.project_name
   #role          = aws_iam_role.lambda_role.arn
-  role          = "arn:aws:iam::211125478754:role/RoleForLambdaModLabRole"
+  #role          = "arn:aws:iam::211125478754:role/RoleForLambdaModLabRole"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "hello.handler"
   runtime       = "nodejs12.x"
   timeout       = 10
