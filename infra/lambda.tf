7037a47c3dbeefcda1926a476ddf48694da40d9e
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-04-12 20:17:49 -0300

    feature: adiciona código de auth na lambda com python

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,5 +1,25 @@
 data "archive_file" "zip" {
   type        = "zip"
+  source_file = "../lambda/lambda-auth.py"
+  output_path = "../lambda/lambda-auth.zip"
+}
+
+resource "aws_lambda_function" "postech-lambda-auth-fastfood" {
+  function_name = var.project_name
+  filename      = data.archive_file.zip.output_path
+  source_code_hash = filebase64sha256(data.archive_file.zip.output_path)
+  role          = var.lab_role_arn
+  runtime       = "python3.6"
+  handler       = "lambda_function.lambda_handler"
+  timeout       = 10
+}
+
+resource "aws_cloudwatch_log_group" "convert_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.postech-lambda-auth-fastfood.function_name}"
+}
+
+/*data "archive_file" "zip" {
+  type        = "zip"
   source_file = "../lambda/hello.js"
   output_path = "../lambda/hello.zip"
 }
@@ -19,4 +39,4 @@ resource "aws_lambda_function" "postech-lambda-auth-fastfood" {
 
 resource "aws_cloudwatch_log_group" "convert_log_group" {
   name = "/aws/lambda/${aws_lambda_function.postech-lambda-auth-fastfood.function_name}"
-}
+}*/
