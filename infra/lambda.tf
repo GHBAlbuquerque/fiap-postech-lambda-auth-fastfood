ba7e96e2995fcda5ed2c2d980bcb06f30383e6a0
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-08 17:44:35 -0300

    feature: sobe codigo simples para teste

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -18,33 +18,7 @@ resource "aws_cloudwatch_log_group" "convert_log_group" {
   name = "/aws/lambda/${aws_lambda_function.postech-lambda-auth-fastfood.function_name}"
 }
 
-resource "aws_lambda_function_url" "lambda_uel" {
+resource "aws_lambda_function_url" "lambda_url" {
   function_name      = aws_lambda_function.postech-lambda-auth-fastfood.function_name
   authorization_type = "NONE"
 }
-
-/*
-  LAMBDA JAVSCRIPT
-
-  data "archive_file" "zip" {
-  type        = "zip"
-  source_file = "../lambda/hello.js"
-  output_path = "../lambda/hello.zip"
-}
-
-resource "aws_lambda_function" "postech-lambda-auth-fastfood" {
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = filebase64sha256(data.archive_file.zip.output_path)
-
-  function_name = var.project_name
-  # role          = aws_iam_role.lambda_role.arn
-  role          = var.lab_role_arn
-  handler       = "hello.handler"
-  runtime       = "nodejs18.x"
-  timeout       = 10
-  # publish       = true
-}
-
-resource "aws_cloudwatch_log_group" "convert_log_group" {
-  name = "/aws/lambda/${aws_lambda_function.postech-lambda-auth-fastfood.function_name}"
-}*/
--- lambda_auth.py
+++ lambda_auth.py
@@ -2,31 +2,48 @@ import boto3
 import json
 
 cognito = boto3.client('cognito-idp')
+
+
 def lambda_handler(event, context):
     print(event)
 
-    username = event['username']
-    password = event['password']
-#    cpf = event['cpf']
+    cpf = event['headers']['cpf_cliente']
+    print(cpf)
 
-    print(username)
-    print(password)
-
-    user_pool_id = 'us-east-1_tDatRvOzb'
-    client_id = 'cliente1'
-    response = cognito.initiate_auth(
-        AuthFlow='USER_PASSWORD_AUTH',
-        AuthParameters={
-            'USERNAME': username,
-            'PASSWORD': password
-        },
-        ClientId=client_id
-    )
+    if (cpf == "123"):
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
 
     return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(response)
+        'statusCode': 403
     }
+
+#     username = event['username']
+#     password = event['password']
+# #    cpf = event['cpf']
+#
+#     print(username)
+#     print(password)
+#
+#     user_pool_id = 'us-east-1_tDatRvOzb'
+#     client_id = 'cliente1'
+#     response = cognito.initiate_auth(
+#         AuthFlow='USER_PASSWORD_AUTH',
+#         AuthParameters={
+#             'USERNAME': username,
+#             'PASSWORD': password
+#         },
+#         ClientId=client_id
+#     )
+#
+#     return {
+#         'statusCode': 200,
+#         'headers': {
+#             'Access-Control-Allow-Origin': '*'
+#         },
+#         'body': json.dumps(response)
+#     }
