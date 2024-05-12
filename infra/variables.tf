af2076d88ef8e41badaba6623d9bad92ff54e86d
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-05-12 11:11:33 -0300

    chore: muda var de lugar

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,7 @@
+variable "tf_state_bucket_key" {
+  default = "terraform-state-backend-postech-new"
+}
+
 variable "project_name" {
   default = "postech-lambda-auth-fastfood"
 }
@@ -12,8 +16,4 @@ variable "profile" {
 
 variable "lab_role_arn" {
   default = "arn:aws:iam::067184771399:role/LabRole"
-}
-
-variable "tf_state_bucket_key" {
-  default = "terraform-state-backend-postech-new"
 }
